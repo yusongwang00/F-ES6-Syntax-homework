c2c04e4ec0059ba3ba4614106fbe0b5fd4cf7571
yusong <yusong.wang@thoughtworks.com>
2020-06-30 00:51:38 +0800

    naive solution need to be improved

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,13 @@
-const parseData
 export { parseData };
+const parseData = function (input) {
+  const res = [];
+  let { data, column } = input;
+  data.map((x) => {
+    const temp = {};
+    column.forEach((element, i) => {
+      temp[element.name] = x[i];
+    });
+    res.push(temp);
+  });
+  return res;
+};
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,20 @@
-const inject
 export { inject };
+const inject = function (item, sections) {
+  let res = [];
+  let sectionsIndex = 0;
+  let itemIndex = 0;
+  let sectionInjectCounter = 0;
+  for (let index = 0; index < item.length + sections.length; index++) {
+    if (
+      sectionsIndex < sections.length &&
+      index === sections[sectionsIndex].index + sectionInjectCounter
+    ) {
+      res[index] = sections[sectionsIndex].content;
+      sectionsIndex++;
+      sectionInjectCounter++;
+    } else {
+      res[index] = item[itemIndex++];
+    }
+  }
+  return res;
+};
